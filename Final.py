bfb08c449ffe3f06d0ceb4fd300ecede9ba9d92e
TimRohr <timothy.rohr@gmail.com>
2015-04-29 08:55:21 -0400

    Deleted EXE

--- Final.py
+++ Final.py
@@ -1,6 +1,5 @@
 # Final Project
 # Timothy Rohr and David Whiskochil
-# GTID's: 903137387 and 901293148
 
 import cv2
 import numpy as np
@@ -21,14 +20,7 @@ urlR = raw_input("Enter Right Image url")
 responseR = requests.get(urlR)
 imR = Image.open(StringIO(responseR.content))
 
-#imL = Image.open("left.JPG")
-#imR = Image.open("right.JPG")
-
-
-#imL.show()
 print "IML Size=", imL.size
-
-#imR.show()
 print "IMR Size=", imR.size
 
 if imL.size < imR.size:
@@ -57,18 +49,8 @@ rsplit_blue[:,:,0]*=0
 rsplit_blue[:,:,1]*=0
 temp_rsplit_blue = Image.fromarray(rsplit_blue)
 
-#rsplit_cyan = np.array(imR)
-#rsplit_cyan[:,:,2]*=0
-#temp_rsplit_cyan = Image.fromarray(rsplit_cyan)
-
-#####temp_lsplit_red.show()
-#####temp_rsplit_blue.show()
-#temp_rsplit_cyan.show()
-
 total_image=lsplit_red+rsplit_blue
 
 total_image = Image.fromarray(total_image)
-######total_image.show()
-#cv2.imwrite("total_image.jpg", total_image)
 total_image.save("total_image.png","PNG")
            
